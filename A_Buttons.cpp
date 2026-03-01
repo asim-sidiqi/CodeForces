848de2c96530ff52fe644c99916b33ccb70c581a
Asim Siddiqui <29aseem.sid@gmail.com>
2026-03-01 20:03:58 +0530

    cf

---
+++ A_Buttons.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int a,b,c;
+        cin>>a>>b>>c;
+
+        if(a+c>b+c) cout<<"First"<<endl;
+        else if(a+c<b+c) cout<<"Second"<<endl;
+        else{
+            if(c%2==0) cout<<"Second"<<endl;
+            else cout<<"First"<<endl;
+        }
+    }
+}
