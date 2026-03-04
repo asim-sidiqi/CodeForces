02726bf5d18d18898c453d3683ce3cb0fcb0deea
Asim Siddiqui <29aseem.sid@gmail.com>
2026-03-04 16:52:56 +0530

    cf

---
+++ A_Twin_Permutations.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int temp=n;
+        vector<int> ans;
+        while(temp--){
+            int x;
+            cin>>x;
+            ans.push_back(n+1-x);
+        }
+
+        for(int it:ans){
+            cout<<it<<" ";
+        }
+        cout<<endl;
+    }
+}
