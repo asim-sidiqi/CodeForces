3d82ac16c17a10d3642fbe0df13b0fe08a087a71
Asim Siddiqui <29aseem.sid@gmail.com>
2026-03-13 23:47:55 +0530

    cf

---
+++ A_One_and_Two.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> arr(n);
+        int countTwo=0;
+        for(int i=0; i<n; i++){
+            cin>>arr[i];
+            if(arr[i]==2) countTwo++;
+        }
+
+        if(countTwo%2!=0){
+            cout<<-1<<endl;
+        }
+        else{
+            int count=0;
+            for(int i=0; i<n; i++){
+                if(arr[i]==2) count++;
+                if(count==countTwo/2){
+                    cout<<i+1<<endl;
+                    break;
+                }
+            }
+        }
+
+        
+    }
+}
