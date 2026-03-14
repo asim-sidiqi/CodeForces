9aa02a8c6e9411f04668f11755d8c1c6b7dacb15
Asim Siddiqui <29aseem.sid@gmail.com>
2026-03-14 21:31:59 +0530

    cf

--- A_One_and_Two.cpp
+++ A_One_and_Two.cpp
@@ -9,6 +9,7 @@ int main(){
         vector<int> arr(n);
         int countTwo=0;
         for(int i=0; i<n; i++){
+            
             cin>>arr[i];
             if(arr[i]==2) countTwo++;
         }
